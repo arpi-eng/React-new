7d865ae5b282d37b3c10a7fa8894e76a8556704f
Abhigyan Dwivedi <84132249+arpi-eng@users.noreply.github.com>
2025-07-06 13:28:34 +0530

    Update Navbar.jsx

--- main/src/components/Navbar.jsx
+++ main/src/components/Navbar.jsx
@@ -1,32 +1,32 @@
-    import React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
 import { DarkMode, LightMode } from '@mui/icons-material';
-import { useThemeContext } from '../context/ThemeContext';
+import { useThemeContext } from '../context/ThemeContext'; // Corrected import path
 
 const Navbar = () => {
   const { mode, toggleTheme } = useThemeContext();
 
   return (
-    <AppBar position="static" elevation={2}>
+    <AppBar position="static" elevation={3}>
       <Toolbar>
         <Typography
           variant="h6"
           component="div"
-          sx={{ flexGrow: 1, fontWeight: 700 }}
+          sx={{ flexGrow: 1, fontWeight: 'bold' }}
         >
           My Dev Journey
         </Typography>
-        <Button component={Link} to="/" color="inherit">
+        <Button color="inherit" component={Link} to="/">
           Home
         </Button>
-        <Button component={Link} to="/projects" color="inherit">
+        <Button color="inherit" component={Link} to="/projects">
           Projects
         </Button>
-        <Button component={Link} to="/about" color="inherit">
+        <Button color="inherit" component={Link} to="/about">
           About
         </Button>
-        <IconButton onClick={toggleTheme} color="inherit">
+        <IconButton color="inherit" onClick={toggleTheme} aria-label="toggle theme">
           {mode === 'dark' ? <LightMode /> : <DarkMode />}
         </IconButton>
       </Toolbar>
